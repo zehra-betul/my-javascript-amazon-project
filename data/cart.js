e47f2f49c9a459c2ed644408f6452da52e04a0ef
zehra-betul <zehrabetul250@gmail.com>
2024-07-31 22:43:23 +0300

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,20 @@
-export let cart=[{
-  productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity:2
-},
-{
-  productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity:2
-}];
+export let cart= JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+  cart = [{
+    productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity:2
+  },
+  {
+    productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity:2
+  }];
+}
+
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId)
 {
@@ -30,6 +39,8 @@ export function addToCart(productId)
       }
     );
   }
+
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -43,4 +54,5 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+  saveToStorage();
 }
