b69008b340c6a70bc01449017647357f8406999e
zehra-betul <zehrabetul250@gmail.com>
2024-08-12 14:21:53 +0300

    Rename jasmine folder to tests

--- tests/tests-simple/moneyTest.js
+++ tests/tests-simple/moneyTest.js
@@ -1,4 +1,4 @@
-import { formatCurrency } from "../scripts/utils/money.js";
+import { formatCurrency } from "../../scripts/utils/money.js";
 
 console.log('TEST SUITE: formatCurrency');
 console.log('converts cents into dollars');
