b73e4eda16c9af1464fe2617284054b8b324f8a1
zehra-betul <zehrabetul250@gmail.com>
2024-08-13 14:10:06 +0300

    Create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
+import '../data/cart-oop.js';//runs all the code inside the file
 
 renderCheckoutHeader();
 renderOrderSummary();
